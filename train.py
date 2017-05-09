0957e39b81ac388c37db087a1706de85054778e2
Kaixhin <design@kaixhin.com>
2017-05-09 16:04:27 +0100

    Revert to normal A3C but with Q

--- train.py
+++ train.py
@@ -55,11 +55,11 @@ def train(rank, args, T, shared_model, optimiser):
       cx = Variable(cx.data)
 
     # Lists of outputs for training
-    Qs, Vs, log_probs, rewards, entropies = [], [], [], [], []
+    Qs, Vs, log_probs, states, actions, rewards, entropies = [], [], [], [], [], [], []
 
     while not done and t - t_start < args.t_max:
-      input = extend_input(state, action_to_one_hot(action, action_size), reward, episode_length)
       # Calculate policy and values
+      input = extend_input(state, action_to_one_hot(action, action_size), reward, episode_length)
       policy, Q, (hx, cx) = model(input, (hx, cx))
       V = (policy * Q).sum(1)  # V is expectation of Q under π
       log_policy = policy.log()
@@ -67,7 +67,9 @@ def train(rank, args, T, shared_model, optimiser):
 
       # Sample action
       action = policy.multinomial().data
-      log_prob = log_policy.gather(1, Variable(action))  # Graph broken as loss for stochastic action calculated manually
+      # Break graph as loss for stochastic action calculated manually
+      log_prob = log_policy.gather(1, Variable(action))  # Log probability of chosen action
+      Q = Q.gather(1, Variable(action))  # Q-value of chosen action
 
       # Step
       next_state, reward, done, _ = env.step(action[0, 0])
@@ -79,6 +81,8 @@ def train(rank, args, T, shared_model, optimiser):
       Qs.append(Q)
       Vs.append(V)
       log_probs.append(log_prob)
+      states.append(input)
+      actions.append(action)
       rewards.append(reward)
       entropies.append(entropy)
 
@@ -91,20 +95,23 @@ def train(rank, args, T, shared_model, optimiser):
       done = done or episode_length >= args.max_episode_length
       state = next_state
 
-    # Return R = 0 for terminal s or V(s_i; θ) for non-terminal s
+    # Break graph for last values calculated (used for targets, not directly as model outputs)
     if done:
+      # R = 0 for terminal s
       R = torch.zeros(1, 1)
+      Q = torch.zeros(1, 1)  # TODO: Q for terminal s is 0, right?
     else:
+      # R = V(s_i; θ) for non-terminal s
       policy, Q, _ = model(input, (hx, cx))
-      policy, Q = policy.data, Q.data
       V = (policy * Q).sum(1)
-      R = V
-      # TODO: Check if the following is correct
+      R = V.data
+      # TODO: Check if following part of if statement is correct
       action = policy.multinomial().data
-      log_prob = policy.log().gather(1, Variable(action))
-      Qs.append(Variable(Q))
+      log_prob = policy.log().gather(1, action).data
+      Q = Q.gather(1, action).data
       log_probs.append(log_prob)
     Vs.append(Variable(R))
+    Qs.append(Variable(Q))
 
     # Train the network
     policy_loss = 0
@@ -116,33 +123,41 @@ def train(rank, args, T, shared_model, optimiser):
     for i in reversed(range(len(rewards))):
       # R ← r_i + γR
       R = rewards[i] + args.discount * R
-      """
       # Advantage A = R - V(s_i; θ)
       A = R - Vs[i]
       # dθ ← dθ - ∂A^2/∂θ
       value_loss += A ** 2
-      """
 
-      # ρ_i+1 = π(a_i+1|s_i+1) / µ(a_i+1|s_i+1)
-      if len(log_probs) <= i + 1:
-        importance_weight = torch.FloatTensor([args.importance_weight_truncation])
-        Qdiff = torch.zeros(1, action_size)
-        Qrets[i + 1] = torch.zeros(1, action_size)
-      else:
-        importance_weight = log_probs[i + 1].data.exp() / 1  # TODO: Where does mu come from?
+      """
+      if len(log_probs) > i + 1:
+        # Importance weight ρ_i+1 = π(a_i+1|s_i+1) / µ(a_i+1|s_i+1)
+        hx = Variable(torch.zeros(1, args.hidden_size))
+        cx = Variable(torch.zeros(1, args.hidden_size))
+        pi, _, _, = model(input, (hx, cx))
+        pi_a = pi.data.gather(1, actions[i + 1])  # TODO: Ideally need to push model forwards through old states?
+        mu_a = log_probs[i + 1].data.exp()  # Data from "old" policy
+        importance_weight = pi_a / mu_a
+        # Off-policy correction Qret(s_i+1, a_i+1) - Q(s_i+1, a_i+1)
         Qdiff = Qrets[i + 1] - Qs[i + 1].data
+      else:
+        # Handle terminal case
+        importance_weight = torch.FloatTensor([args.importance_weight_truncation])
+        Qdiff = torch.zeros(1, 1)  # TODO: Assume no off-policy correction on terminal s?
+        Qrets[i + 1] = torch.zeros(1, 1)  # TODO: Q on terminal s is 0, correct?
+
       # ρ¯_i+1 = min(c, ρ_i+1)
-      truncated_importance_weight = torch.clamp(importance_weight, min=args.importance_weight_truncation)
+      truncated_importance_weight = torch.clamp(importance_weight, min=args.max_trace)
       # Qret(s_i, a_i) = r_i + γρ¯_i+1[Qret(s_i+1, s_t+1) − Q(s_t+1, s_t+1)] + γV(s_t+1)
-      Qrets[i] = rewards[i] + args.discount * truncated_importance_weight.expand_as(Qdiff) * \
-          Qdiff + args.discount * Vs[i + 1].data.expand_as(Qdiff)
+      Qrets[i] = rewards[i] + args.discount * truncated_importance_weight * \
+          Qdiff + args.discount * Vs[i + 1].data
       # dθ ← dθ - (Qret(s_i, a_i) - Q(s_i, a_i))∇θ∙Q(s_i, a_i)
       value_loss += (Variable(Qrets[i]) - Qs[i]).mean()  # TODO: Should operate only a of Q?
+      """
 
       # TD residual δ = r_i + γV(s_i+1; θ) - V(s_i; θ)
       td_error = rewards[i] + args.discount * Vs[i + 1].data - Vs[i].data
       # Generalised advantage estimator Ψ (roughly of form ∑(γλ)^t∙δ)
-      A_GAE = A_GAE * args.discount * args.gae_discount + td_error
+      A_GAE = A_GAE * args.discount * args.trace_decay + td_error
       # dθ ← dθ + ∇θ∙log(π(a_i|s_i; θ))∙Ψ - β∙∇θH(π(s_i; θ))
       policy_loss += -log_probs[i] * Variable(A_GAE) + args.entropy_weight * entropies[i]
 
