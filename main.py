Fix KL div?

main.py
@@ -33,7 +33,7 @@ parser.add_argument('--trust-region-decay', type=float, default=0.99, metavar='�
 parser.add_argument('--trust-region-threshold', type=float, default=1, metavar='δ', help='Trust region threshold value')
 parser.add_argument('--reward-clip', action='store_true', help='Clip rewards to [-1, 1]')
 parser.add_argument('--lr', type=float, default=1e-3, metavar='η', help='Learning rate')
-parser.add_argument('--lr-decay', action='store_true', help='Linearly decay learning rate to 0')  # TODO: See if this is better on by default
+parser.add_argument('--lr-decay', action='store_true', help='Linearly decay learning rate to 0')
 parser.add_argument('--rmsprop-decay', type=float, default=0.99, metavar='α', help='RMSprop decay factor')
 parser.add_argument('--batch-size', type=int, default=16, metavar='SIZE', help='Off-policy batch size')
 parser.add_argument('--entropy-weight', type=float, default=0.001, metavar='β', help='Entropy regularisation weight')
@@ -46,15 +46,17 @@ parser.add_argument('--render', action='store_true', help='Render evaluation age
 
 
 if __name__ == '__main__':
-  # OS env setup
+  # BLAS setup
   os.environ['OMP_NUM_THREADS'] = '1'
-  os.environ['OPENAI_REMOTE_VERBOSE'] = '0'
+  os.environ['MKL_NUM_THREADS'] = '1'
+  # os.environ['OPENAI_REMOTE_VERBOSE'] = '0'
   
   # Setup
   args = parser.parse_args()
   print(' ' * 26 + 'Options')
   for k, v in vars(args).items():
     print(' ' * 26 + k + ': ' + str(v))
+  args.env = 'CartPole-v1'  # TODO: Remove hardcoded environment when code is more adaptable
   torch.manual_seed(args.seed)
   T = Counter()  # Global shared counter
 

train.py
@@ -4,6 +4,7 @@ import random
 import gym
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch.autograd import Variable
 
 from memory import EpisodicReplayMemory
@@ -65,7 +66,7 @@ def _trust_region_loss(model, distribution, ref_distribution, loss, threshold):
   model.zero_grad()
 
   # KL divergence k ← ∇θ0∙DKL[π(∙|s_i; θ_a) || π(∙|s_i; θ)]
-  kl = (ref_distribution * (ref_distribution.log() - distribution.log())).sum()
+  kl = F.kl_div(distribution.log(), ref_distribution, size_average=False)
   # Compute gradients from (negative) KL loss (increases KL divergence)
   (-kl).backward(retain_graph=True)
   k = [Variable(param.grad.data.clone()) for param in model.parameters()]
@@ -76,7 +77,7 @@ def _trust_region_loss(model, distribution, ref_distribution, loss, threshold):
   k_dot_k = sum(torch.sum(k_p ** 2) for k_p in k)
   # Compute trust region update
   if k_dot_k.data[0] > 0:
-    trust_factor = (k_dot_g - threshold) / k_dot_k
+    trust_factor = ((k_dot_g - threshold) / k_dot_k).clamp(min=0)
   else:
     trust_factor = Variable(torch.zeros(1))
   # z* = g - max(0, (k^T∙g - δ) / ||k||^2_2)∙k