df11f86740ceceb5f1599efdd99a12a279fab084
Feryal <feryal.mp@gmail.com>
2017-06-26 19:53:23 +0100

    entropy cost sign changed as the entropy should be maximised to stimulate exploration

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ def _train(args, T, model, shared_model, shared_average_model, optimiser, polici
       policy_loss += single_step_policy_loss
 
     # Entropy regularisation dθ ← dθ - β∙∇θH(π(s_i; θ))
-    policy_loss += args.entropy_weight * -(policies[i].log() * policies[i]).sum(1).mean(0)
+    policy_loss -= args.entropy_weight * -(policies[i].log() * policies[i]).sum(1).mean(0)
 
     # Value update dθ ← dθ - ∇θ∙1/2∙(Qret - Q(s_i, a_i; θ))^2
     Q = Qs[i].gather(1, actions[i])
